83882eb0ba1b27ff58eed4503173dbda05cb64ee
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 08:42:16 +0700

    Integer

---
+++ src/pbo/pkg10119911/latihan7/integer/Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg101119911.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911 
+ * Deskripsi Program : Integer
+ */
+public class Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
